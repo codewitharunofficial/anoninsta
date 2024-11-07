f36d68ca5c566b19384103b85a735374c5214312
Arun <133745182+codewitharunofficial@users.noreply.github.com>
2024-11-07 14:27:21 +0530

    Update SearchBar.jsx

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import axios from "axios";
 import { useUser } from "@/context/UserConext";
+import Loader from "./Loader";
 
 const SearchBar = ({ route, setRoute }) => {
   const [userName, setUserName] = useState("");
